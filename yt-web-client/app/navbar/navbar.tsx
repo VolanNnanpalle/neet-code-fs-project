5b45042823f47de308785d9ac6c4aa3fb0d3b983
Volan Nnanpalle <vnnanpalle3@gatech.edu>
2023-12-20 00:16:32 -0500

    completed SignIn with Google

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,8 +1,24 @@
+'use client'
+
 import Image from "next/image"; //react element
 import styles from "./navbar.module.css";
 import Link from "next/link";
-
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
 export default function Navbar() {
+    //init user state
+    const [user,setUser] = useState<User| null>(null)
+
+    useEffect(() =>{
+        const unsubscribed = onAuthStateChangedHelper((user)=>{
+            setUser(user)
+        })
+        //cleanup subscription on unmount
+        return () => unsubscribed();    
+    })
+
     return (
         <nav className={styles.nav}>
             <Link href="/" className= {styles.logoContainer}>
@@ -10,6 +26,10 @@ export default function Navbar() {
                     src="/youtube-logo.svg" alt="Youtube Logo">
                 </Image>
             </Link>
+            {
+                //  TODO: Add upload button 
+            }
+            <SignIn user={user}/>
         </nav>
     );
 }
